c2c964161a3dee29d9f8b765ac3cf6865f499702
nahuelSantiagoBC <nahuel.santiago@insbaixcamp.cat>
2020-11-01 19:17:21 +0100

    Muchos cambios. Te los explico en clase ':D

--- src/chess_project/Board.java
+++ src/chess_project/Board.java
@@ -18,43 +18,43 @@ public class Board {
 	public void initBoard() {
 		
 		//inicializamos whites
-		board[0][0] = new Token(false, "white", 'T');
-		board[0][1] = new Token(false, "white", 'C');
-		board[0][2] = new Token(false, "white", 'A');
-		board[0][3] = new Token(false, "white", 'Q');
-		board[0][4] = new Token(false, "white", 'K');
-		board[0][5] = new Token(false, "white", 'A');
-		board[0][6] = new Token(false, "white", 'C');
-		board[0][7] = new Token(false, "white", 'T');
+		board[0][0] = new Token('T');
+		board[0][1] = new Token('C');
+		board[0][2] = new Token('A');
+		board[0][3] = new Token('Q');
+		board[0][4] = new Token('K');
+		board[0][5] = new Token('A');
+		board[0][6] = new Token('C');
+		board[0][7] = new Token('T');
 		
-		board[1][0] = new Token(false, "white", 'P');
-		board[1][1] = new Token(false, "white", 'P');
-		board[1][2] = new Token(false, "white", 'P');
-		board[1][3] = new Token(false, "white", 'P');
-		board[1][4] = new Token(false, "white", 'P');
-		board[1][5] = new Token(false, "white", 'P');
-		board[1][6] = new Token(false, "white", 'P');
-		board[1][7] = new Token(false, "white", 'P');
+		board[1][0] = new Token('P');
+		board[1][1] = new Token('P');
+		board[1][2] = new Token('P');
+		board[1][3] = new Token('P');
+		board[1][4] = new Token('P');
+		board[1][5] = new Token('P');
+		board[1][6] = new Token('P');
+		board[1][7] = new Token('P');
 		
 		
 		//inicializamos negras
-		board[7][0] = new Token(false, "black", 't');
-		board[7][1] = new Token(false, "black", 'c');
-		board[7][2] = new Token(false, "black", 'a');
-		board[7][3] = new Token(false, "black", 'q');
-		board[7][4] = new Token(false, "black", 'k');
-		board[7][5] = new Token(false, "black", 'a');
-		board[7][6] = new Token(false, "black", 'c');
-		board[7][7] = new Token(false, "black", 't');
+		board[7][0] = new Token('t');
+		board[7][1] = new Token('c');
+		board[7][2] = new Token('a');
+		board[7][3] = new Token('q');
+		board[7][4] = new Token('k');
+		board[7][5] = new Token('a');
+		board[7][6] = new Token('c');
+		board[7][7] = new Token('t');
 		
-		board[6][0] = new Token(false, "black", 'p');
-		board[6][1] = new Token(false, "black", 'p');
-		board[6][2] = new Token(false, "black", 'p');
-		board[6][3] = new Token(false, "black", 'p');
-		board[6][4] = new Token(false, "black", 'p');
-		board[6][5] = new Token(false, "black", 'p');
-		board[6][6] = new Token(false, "black", 'p');
-		board[6][7] = new Token(false, "black", 'p');
+		board[6][0] = new Token('p');
+		board[6][1] = new Token('p');
+		board[6][2] = new Token('p');
+		board[6][3] = new Token('p');
+		board[6][4] = new Token('p');
+		board[6][5] = new Token('p');
+		board[6][6] = new Token('p');
+		board[6][7] = new Token('p');
 		
 		//Autocompletamos lo que queda del tablero
 		
@@ -62,19 +62,37 @@ public class Board {
 			System.out.println();
 			for (int j = 0; j <= 8; j++) {
 				
-				board[i][j] = null;
+				board[i][j] = new Token();
 				//Esto es un intento de intentar printar el tipo de ficha que es pero como es una matriz de objetos, que guar un objeto posicion y luego guarda un objeto ficha no se
 				//como pillar el caracter de la ficha
-				System.out.println(getTokenType(i, j));
+				System.out.println(getToken(new int[]{i, j}));
 			}
 			
 		}
 	}
 	
-	public char getTokenType(int x, int y) {
+	public Token getToken(int[] tokenLocation) {
 		
-		return board[x][y].getType();
+		return board[tokenLocation[0]][tokenLocation[1]];
 	}
+	
+	//funciones
+	
+	public boolean checkMove(int[] initLocation, int[] targetLocation) {
+		Token token = getToken(initLocation);
+		boolean result = false;
+		//booleana que comprueba que el lugar al que se quiere mover está en la tabla
+		boolean offBoard = (targetLocation[0] > 7 || targetLocation[0] < 0 || targetLocation[1] > 7 || targetLocation[1] < 0);
+		
+		if(!offBoard) {
+			if (token.checkTokenPattern(targetLocation)) {
+				
+			}
+		}
+		
+		return result;
+	}
+	
 	
 	
 }
--- src/chess_project/Token.java
+++ src/chess_project/Token.java
@@ -4,38 +4,147 @@ public class Token {
 
 	//propiedades de la ficha
 	boolean isCaptured;
-	String color;
+	boolean isCapturing;
 	char type;
+	MoveController pattern;
+	int[] currentLocation;
 	
 	//construstores
-	Token(boolean isCaptured, String color, char type){
+	public Token(char type){
 		
-		this.isCaptured = isCaptured;
-		this.color = color;
+		this.isCaptured = false;
 		this.type = type;
+		pattern = new MoveController(this);
+	}
+	
+	public Token() {
+		type = 'e';
 	}
 
+	
+	//funciones
+	
+	public boolean checkTokenPattern(int[] targetLocation) {
+		boolean result = false;
+		boolean movedOneUp;
+		boolean movedOneDown;
+		boolean movedOneRight;
+		boolean movedOneLeft;
+		boolean movedDiagonally;
+		boolean movedHorizontally;
+		boolean movedVertically;
+		
+		
+		switch (Character.toLowerCase(type)) {
+			case 'p':
+				
+				movedOneUp = (targetLocation[1] == currentLocation[1]+1) && type == 'P';
+				movedOneDown = (targetLocation[1] == currentLocation[1]-1) && type == 'p';
+
+				if (isCapturing) {
+					movedOneRight = (targetLocation[0] == currentLocation[0]+1);
+					movedOneLeft = (targetLocation[0] == currentLocation[0]-1);
+					
+					//comprobamos si se ha movido en diagonal 1 puesto
+					result = (movedOneDown && movedOneRight) || (movedOneDown && movedOneLeft) || (movedOneUp && movedOneRight) || (movedOneUp && movedOneLeft);
+				} else {
+					//comprobamos si está en la segunda o penúltima línea
+					if (currentLocation[1] == 1 || currentLocation[1] == 7) {
+						boolean movedTwoUp = (targetLocation[1] == currentLocation[1]+2) && type == 'P';
+						boolean movedTwoDown = (targetLocation[1] == currentLocation[1]-2) && type == 'p';
+						
+						result = movedOneDown || movedOneUp || movedTwoDown || movedTwoUp;
+					} else {
+						result = movedOneDown || movedOneUp;
+					}
+				}
+				
+				break;
+	
+			case 'a':
+				
+				movedDiagonally = targetLocation[0] - currentLocation[0] == targetLocation[1] - currentLocation[1];
+				
+				result =movedDiagonally;
+				
+				break;
+	
+			case 'c':
+				
+				boolean jumpedUpLeft = (targetLocation[0] == currentLocation[0] + 1 && targetLocation[1] == currentLocation[1] + 2);
+				boolean jumpedLeftUp = (targetLocation[0] == currentLocation[0] + 2 && targetLocation[1] == currentLocation[1] + 1);
+				boolean jumpedUpRight = (targetLocation[0] == currentLocation[0] - 1 && targetLocation[1] == currentLocation[1] + 2);
+				boolean jumpedRightUp = (targetLocation[0] == currentLocation[0] - 2 && targetLocation[1] == currentLocation[1] + 1);
+				boolean jumpedDownLeft = (targetLocation[0] == currentLocation[0] + 1 && targetLocation[1] == currentLocation[1] - 2);
+				boolean jumpedLeftDown = (targetLocation[0] == currentLocation[0] + 2 && targetLocation[1] == currentLocation[1] - 1);
+				boolean jumpedDownRight = (targetLocation[0] == currentLocation[0] - 1 && targetLocation[1] == currentLocation[1] - 2);
+				boolean jumpedRightDown = (targetLocation[0] == currentLocation[0] - 2 && targetLocation[1] == currentLocation[1] - 1);
+				
+				result = jumpedDownLeft || jumpedDownRight || jumpedLeftDown || jumpedLeftUp  || jumpedRightDown || jumpedRightUp || jumpedUpLeft || jumpedUpRight;
+				
+				break;
+	
+			case 't':
+				
+				movedHorizontally = (targetLocation[0] != currentLocation[0]) && (targetLocation[1] == currentLocation[1]);
+				movedVertically = (targetLocation[0] == currentLocation[0]) && (targetLocation[1] != currentLocation[1]);
+				
+				result = movedVertically || movedHorizontally;
+				
+				break;
+	
+			case 'q':
+				
+				movedDiagonally = targetLocation[0] - currentLocation[0] == targetLocation[1] - currentLocation[1];
+				movedHorizontally = (targetLocation[0] != currentLocation[0]) && (targetLocation[1] == currentLocation[1]);
+				movedVertically = (targetLocation[0] == currentLocation[0]) && (targetLocation[1] != currentLocation[1]);
+				
+				result = movedDiagonally || movedHorizontally || movedVertically;
+				
+				break;
+			
+			case 'k':
+				
+				movedOneUp = (targetLocation[1] == currentLocation[1]+1) && type == 'P';
+				movedOneDown = (targetLocation[1] == currentLocation[1]-1) && type == 'p';
+				movedOneRight = (targetLocation[0] == currentLocation[0]+1);
+				movedOneLeft = (targetLocation[0] == currentLocation[0]-1);
+				
+				result = movedOneDown || movedOneLeft || movedOneRight || movedOneUp;
+				
+				break;
+	
+			default:
+				break;
+		}
+		
+		return result;
+	}
+	
+	//getters y setters
+	
 	public boolean isCaptured() {
 		return isCaptured;
 	}
 
-	public String getColor() {
-		return color;
+	public boolean isCapturing() {
+		return isCapturing;
 	}
 
+	
 	public char getType() {
 		return type;
 	}
 
-	public void setMuerta(boolean isCaptured) {
+	public void setCaptured(boolean isCaptured) {
 		this.isCaptured = isCaptured;
 	}
-
-	public void setColor(String color) {
-		this.color = color;
+	
+	public void setCapturing(boolean isCapturing) {
+		this.isCapturing = isCapturing;
 	}
 
-	public void setTipo(char type) {
+	public void setType(char type) {
 		this.type = type;
 	}
 	
